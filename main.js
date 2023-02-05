0c37536f82c608ce6467d770a594663ffc0f850d
Your Name <m.urdaneta@correo.unimet.edu.ve>
2023-02-05 13:56:03 -0400

    Responsividad y ultimos toques

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
+let menuVisible = false;
+//Efectos para el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
 
+function seleccionar(){
+    //Hide menu una vez que selecciona una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//JSON para los skills
+function efectoSkills(){
+    var skills = document.getElementById("skills");
+    var lenght_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(lenght_skills >= 300){
+        let skills = document.getElementsByClassName("progress");
+        skills[0].classList.add("javascript");
+        skills[1].classList.add("htmlcss");
+        skills[2].classList.add("photoshop");
+        skills[3].classList.add("python");
+        skills[4].classList.add("java");
+        skills[5].classList.add("comunicacion");
+        skills[6].classList.add("trabajo");
+        skills[7].classList.add("creatividad");
+        skills[8].classList.add("dedicacion");
+        skills[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
